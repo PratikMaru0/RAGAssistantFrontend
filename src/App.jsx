d3ac00ffdc0033de41163a989b46a9577c7cc92d
Pratik maru <pratikmaru80762@example.com>
2025-09-29 12:56:48 +0530

    fix reloead page fail issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+// src/App.jsx
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// Change this import
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Chatbot from "./aiUI/Chatbot.jsx";
 import Context from "./aiUI/Context";
 
 const App = () => {
   return (
+    // And use the Router here (which is now HashRouter)
     <Router>
       <div className="h-screen bg-gray-900">
         <Routes>
