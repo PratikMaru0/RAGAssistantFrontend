a4137e24ad8af697aee3f28c1f99e3a3a6c8bf2b
Pratik maru <pratikmaru80762@example.com>
2025-09-29 12:10:25 +0530

    fixed build error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Chatbot from "./aiUI/chatbot.jsx";
+import Chatbot from "./aiUI/Chatbot.jsx";
 import Context from "./aiUI/Context";
 
 const App = () => {
